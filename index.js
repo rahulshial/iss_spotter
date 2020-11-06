9d0420a2861e2920ae96dc4887ddb2a738ee1c43
Rahul <rahul.shial@gmail.com>
2020-11-06 03:06:37 +0000

    merged all functions to print out next 5 passes

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// const ip = '70.77.2.242';
+
+// fetchCoordsByIP(ip, (error, coords) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coords:' , coords);
+// });
+
+// const coords = { latitude: 50.9129, longitude: -114.1028 };
+
+// fetchISSFlyOverTimes(coords, (error, riseTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coords:' , riseTimes);
+// });
+
+const printFlyOvers = function(passTimes) {
+  for (const pass of passTimes) {
+    const dateTime = new Date(0);
+    dateTime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${dateTime} for ${duration} seconds!`);
   }
-  console.log('It worked! Returned IP:' , ip);
-});
+};
 
-const ip = '70.77.2.242';
-
-fetchCoordsByIP(ip, (error, coords) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log('It worked! Returned coords:' , coords);
+  // success, print out the deets!
+  printFlyOvers(passTimes);
 });
-
-const coords = { latitude: 50.9129, longitude: -114.1028 };
-
-fetchISSFlyOverTimes(coords, (error, riseTimes) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log('It worked! Returned coords:' , riseTimes);
-});
-
